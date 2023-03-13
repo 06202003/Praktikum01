f598dcc8fd42624f5bcd9aeec74b06a62a2bfc0c
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2023-03-13 15:54:09 +0700

    Delete util_function.php

--- util_function.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   function createMySQLConnection(): PDO{
-    $link = new PDO('mysql:host=localhost;dbname=pwl20222','root','');
-    $link -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $link -> setAttribute(PDO::ATTR_AUTOCOMMIT,false);
-    return $link;
-   } 
-?>
